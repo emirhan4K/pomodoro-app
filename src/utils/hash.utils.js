3d68d97897cd7a89e09f4dbe931b28b7ec569518
Emirhan <emirhan.bda0@gmail.com>
2026-05-01 10:40:25 +0300

    .

---
+++ src/utils/hash.utils.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcrypt');
+
+const hashPassword = async (password) => { //Şifre hashleme fonksiyonu
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (password, hashedPassword) => { //Şifre karşılaştırma fonksiyonu
+    return await bcrypt.compare(password, hashedPassword);
+};
+
+module.exports = { hashPassword, comparePassword };
---
+++ src/utils/jwt.utils.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (payload) => { //JWT oluşturma fonksiyonu
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+};
+ 
+const verifyToken = (token) => { //JWT doğrulama fonksiyonu
+    return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+module.exports = { generateToken, verifyToken };
