b6f8ce8739eedf2ce000e88ca7fa4184ab0b5c4b
Emirhan <emirhan.bda0@gmail.com>
2026-05-01 15:40:54 +0300

    .

--- src/services/profile.service.js
+++ src/services/profile.service.js
@@ -7,16 +7,16 @@ class ProfileService {
     this.profileRepository = profileRepository;
     this.userRepository = userRepository;
   }
-  async getUserProfile(userId) {
+  async getUserProfile(userId) { //Kullanıcının kendi bilgilerini getirir
     const user = await this.userRepository.findById(userId);
     const profile = await this.profileRepository.findByUserId(userId);
     
     return ProfileMapper.toResponse(user, profile);
   }
-  async handleCompletedPomodoro(userId, duration) {
+  async handleCompletedPomodoro(userId, duration) { //Tamamlanan bir pomodoro sonrası istatistikleri günceller
     return await this.profileRepository.updateStats(userId, duration);
   }
-  async getPublicProfile(targetUserId) {
+  async getPublicProfile(targetUserId) { //Kullanıcının profilini başkaları dışarıdan görüntüler
   const user = await this.userRepository.findById(targetUserId);
   if (!user) {
     throw new BadRequestException("Kullanıcı bulunamadı!");
