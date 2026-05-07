449fa3b089e2fce93788f847692ba5080c89f34c
Emirhan <emirhan.bda0@gmail.com>
2026-05-07 13:51:21 +0300

    .

--- src/server.js
+++ src/server.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./config/db');
 
-require('./config/redis');
 require('./config/db');
 
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     await connectDB();
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
         console.log(`✅ Sunucu ${PORT} Portunda Başarıyla Ayağa Kalktı!`);
     });
 };
