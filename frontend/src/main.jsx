56fdba85b1b269c1440d325789f8dfbcb51e66bf
Emirhan <emirhan.bda0@gmail.com>
2026-05-09 14:28:03 +0300

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { AuthProvider } from './context/AuthContext.jsx'
-import { PomodoroProvider } from './context/PomodoroContext.jsx' 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { PomodoroProvider } from "./context/PomodoroContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <PomodoroProvider> 
-        <App />
-      </PomodoroProvider>
-    </AuthProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <PomodoroProvider>
+      <App />
+    </PomodoroProvider>
+  </AuthProvider>,
+);
